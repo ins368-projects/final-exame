ac0397f0bc0e2594dd30ad091e5b18219b36247e
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2020-07-16 22:27:47 -0400

    Implement app

--- PotentiationFinalExam/PotentiationfinalExam.App/Program.cs
+++ PotentiationFinalExam/PotentiationfinalExam.App/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using static System.Console;
+using PotentiationFinalExam.Pow;
 
 namespace PotentiationfinalExam.App
 {
@@ -6,7 +8,16 @@ namespace PotentiationfinalExam.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Powlib app (calcula potencias de números)\n---\n");
+
+            Write("Digite primer número: ");
+            var number1 = Double.Parse(ReadLine());
+
+            Write("Digite segundo número: ");
+            var number2 = Double.Parse(ReadLine());
+
+            double result = Powlib.Pow(number1, number2);
+            WriteLine($"\nResultado: {result}");
         }
     }
 }
