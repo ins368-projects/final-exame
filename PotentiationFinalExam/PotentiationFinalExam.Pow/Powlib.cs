727efae1ccdb7b1b6fff488daa75a2d1e30d46ef
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2020-07-16 22:20:42 -0400

    Negative cases are passing

--- PotentiationFinalExam/PotentiationFinalExam.Pow/Powlib.cs
+++ PotentiationFinalExam/PotentiationFinalExam.Pow/Powlib.cs
@@ -9,8 +9,10 @@ namespace PotentiationFinalExam.Pow
             double maxValue = double.MaxValue;
             double result = Math.Pow(a, b);
 
-            if(Double.IsInfinity(result))
+            if(Double.IsPositiveInfinity(result))
                 throw new System.InvalidOperationException("Número está fuera de rango positivo");
+            else if(Double.IsNegativeInfinity(result))
+                throw new System.InvalidOperationException("Número está fuera de rango negativo");
             
             return result;
         }
--- PotentiationFinalExam/PotentiationFinalExam.Tests/TestPowlib.cs
+++ PotentiationFinalExam/PotentiationFinalExam.Tests/TestPowlib.cs
@@ -20,8 +20,8 @@ namespace PotentiationFinalExam.Tests
         [Theory]
         [InlineData(double.MaxValue, 100, "Número está fuera de rango positivo")]
         [InlineData(double.MaxValue, 3, "Número está fuera de rango positivo")]
-        [InlineData(double.MinValue, 100, "Número está fuera de rango negativo")]
-        [InlineData(double.MinValue, 3, "Número está fuera de rango negativo")]
+        [InlineData(double.MinValue, 101, "Número está fuera de rango negativo")]
+        [InlineData(double.MinValue, 5, "Número está fuera de rango negativo")]
         public void NumberIsNotInDoubleRange(double a, double b, string expectedResult)
         {
             double maxValue = double.MaxValue; 
